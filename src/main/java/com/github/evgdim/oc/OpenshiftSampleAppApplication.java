306c32ca4a05a52995585ecf30d1d17869a54fb7
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2018-02-12 19:36:56 +0200

    initial

---
+++ src/main/java/com/github/evgdim/oc/OpenshiftSampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.github.evgdim.oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftSampleAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/evgdim/oc/OpenshiftSampleAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.evgdim.oc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenshiftSampleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
