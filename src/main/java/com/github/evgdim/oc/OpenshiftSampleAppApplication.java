f967f89753ba0e44dbf195fb2c42b3f256ba9ef4
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2018-02-12 21:23:58 +0200

    added servlet initializer

--- src/main/java/com/github/evgdim/oc/OpenshiftSampleAppApplication.java
+++ src/main/java/com/github/evgdim/oc/OpenshiftSampleAppApplication.java
@@ -2,11 +2,18 @@ package com.github.evgdim.oc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OpenshiftSampleAppApplication {
+public class OpenshiftSampleAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftSampleAppApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OpenshiftSampleAppApplication.class);
 	}
 }
