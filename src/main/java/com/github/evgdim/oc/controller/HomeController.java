39742f9817fd761a3acce1c31e305b380acf5b9e
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2018-04-09 21:59:15 +0300

    xml response test

--- src/main/java/com/github/evgdim/oc/controller/HomeController.java
+++ src/main/java/com/github/evgdim/oc/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.github.evgdim.oc.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import com.github.evgdim.oc.model.Message;
 
 @RestController
 public class HomeController {
-	@GetMapping
+	@GetMapping(produces = { MediaType.APPLICATION_XML_VALUE })
 	public Message index() {
 		return new Message("OK");
 	}
